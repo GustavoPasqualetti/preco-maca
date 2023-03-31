668eec46107ce7d52e79136bf60e25fe83266adc
Gustavo Pasqualetti <49802004863@info.local>
2023-03-31 08:15:22 -0300

    projeto precos maca

---
+++ maças-projeto/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write($"Qantas maças você deseja comprar ");
+double maca= double.Parse(Console.ReadLine());
+
+double menor= (maca * 0.30);
+double maior= (maca * 0.25);
+
+if ( maca < 12)
+{
+Console.WriteLine($"sua compra deu: {menor}R$ ");
+
+}
+
+else 
+{
+Console.WriteLine($"Sua compra deu: {maior}R$ ");
+
+}
